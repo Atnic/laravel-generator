d66786c0c760e66f897c262dd57c3c4146b7f279
Farid Inawan <frdteknikelektro@gmail.com>
2018-01-22 15:17:58 +0700

    Update view stubs

--- config/generator.php
+++ config/generator.php
@@ -3,11 +3,9 @@
 return [
 
     /**
-     *  View Namespace, don't forgot trailing '::'
+     *  View Component Namespace, don't forgot trailing '::'
      *  Leave it blank if you use your own view
      */
-    'view' => 'generator::',
-
     'view_component' => 'generator::'
 
 ];
