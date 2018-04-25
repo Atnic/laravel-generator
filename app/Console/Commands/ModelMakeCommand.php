5618e210fa0d16de6ad5ded7f561723eae859144
Farid Inawan <frdteknikelektro@gmail.com>
2018-04-25 13:56:49 +0700

    Add translation generations

--- app/Console/Commands/ModelMakeCommand.php
+++ app/Console/Commands/ModelMakeCommand.php
@@ -19,7 +19,9 @@ class ModelMakeCommand extends Command
     {
         $this->createFilter();
 
-        return parent::handle();
+        if (parent::handle() === false) return false;
+
+        $this->generateTranslation();
     }
 
     /**
@@ -65,5 +67,76 @@ class ModelMakeCommand extends Command
         }
 
         return __DIR__.'/stubs/model.stub';
+    }
+
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getTranslationStub()
+    {
+        return __DIR__.'/stubs/translation.stub';
+    }
+
+    /**
+     * Generate Translation File
+     * @return void
+     */
+    protected function generateTranslation()
+    {
+        $name = $this->qualifyClass($this->getNameInput());
+        $path = $this->getTranslationPath($name);
+
+        $this->makeDirectory($path);
+        $this->files->put($path, $this->buildTranslation($name));
+
+        $this->info('Translation also generated successfully.');
+        $this->warn($path);
+    }
+
+    /**
+     * Get the translation path.
+     *
+     * @param  string  $name
+     * @return string
+     */
+    protected function getTranslationPath($name)
+    {
+        $name = $this->getTranslationName($name);
+
+        return base_path().'/resources/lang/en/'.$name.'.php';
+    }
+
+    /**
+     * Get the translation name.
+     *
+     * @param  string  $name
+     * @return string
+     */
+    protected function getTranslationName($name)
+    {
+        $name = Str::replaceFirst($this->getDefaultNamespace(trim($this->rootNamespace(), '\\')).'\\', '', $name);
+        $name = Str::replaceLast('Models\\', '', $name);
+        $name = strtolower(str_plural($name));
+
+        return $name;
+    }
+
+
+    /**
+     * Build the translation with the given name.
+     *
+     * @param  string  $name
+     * @return string
+     */
+    protected function buildTranslation($name)
+    {
+        $replace = [
+            'dummy_model_plural_variable' => str_plural(snake_case(class_basename($name), ' ')),
+            'dummy_model_variable' => snake_case(class_basename($name), ' '),
+        ];
+
+        return str_replace(array_keys($replace), array_values($replace), $this->files->get($this->getTranslationStub()));
     }
 }
