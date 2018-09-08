61895a226f736dfae2f37d1ec5178ccf97bc7bb6
Farid Inawan <frdteknikelektro@gmail.com>
2018-09-09 06:57:14 +0700

    Add Store and Create again on nested

--- resources/views/components/models/childs/create.blade.php
+++ resources/views/components/models/childs/create.blade.php
@@ -1,17 +1,17 @@
 @section('parent-content')
-    <form class="form" action="{{ route($resource_route.'.store', [
-        $parent->getKey(),
-        'redirect' => route($resource_route.'.index', [
-            $parent->getKey(),
-            'redirect' => request()->filled('redirect') ? request()->redirect : null ]) ]) }}" method="POST">
+    <form class="form" action="{{ route($resource_route.'.store', [ $parent->getKey() ]) }}" method="POST">
         {{ csrf_field() }}
         @foreach ($fields[$model_variable] as $key => $field)
             @component(config('generator.view_component').'components.fields.'.$field['field'], compact('field'))
             @endcomponent
         @endforeach
         <div class="pull-right">
-            <button type="submit" class="btn btn-primary">
+            <button type="submit" name="redirect" value="{{ route($resource_route.'.index', [ $parent->getKey(),
+                'redirect' => request()->filled('redirect') ? request()->redirect : null ]) }}" class="btn btn-primary">
                 {{ __('Store') }}
+            </button>
+            <button type="submit" name="redirect" value="{{ request()->fullUrl() }}" class="btn btn-primary">
+                {{ __('Store') }} & {{ __('Create') }}
             </button>
         </div>
         @if (Route::has($resource_route.'.index'))
