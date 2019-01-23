ba82c2ecb8991a98efb17d10aed663f95e2eb521
Farid Inawan <frdteknikelektro@gmail.com>
2019-01-23 19:22:56 +0700

    Now support input with array name convert 'name.tags' to 'name[tags]'.

--- resources/views/components/fields/select.blade.php
+++ resources/views/components/fields/select.blade.php
@@ -1,13 +1,18 @@
+@php
+    $names = explode('.', $field['name']);
+    $name = array_shift($names).(count($names) > 0 ? '['.collect($names)->map(function ($name) { return $name == '*' ? $name : ''; })->implode('][').']' : '');
+@endphp
+
 <div class="form-group{{ $errors->has($field['name']) ? ' has-error' : '' }}">
-    <label class="control-label">{{ !empty($field['label']) ? $field['label'] : ucwords(str_replace('_', ' ', snake_case($field['name']))) }}{{ !empty($field['required']) ? '*' : '' }}</label>
-    <select class="form-control{{ $errors->has($field['name']) ? ' is-invalid' : '' }}" name="{{ $field['name'] }}"
+    <label class="control-label" for="{{ $field['name'] }}">{{ !empty($field['label']) ? $field['label'] : ucwords(str_replace('_', ' ', snake_case($field['name']))) }}{{ !empty($field['required']) ? '*' : '' }}</label>
+    <select id="{{ $field['name'] }}" class="form-control{{ $errors->has($field['name']) ? ' is-invalid' : '' }}" name="{{ $field['name'] }}"
         title="{{ $field['name'] }}"
         {{ !empty($field['readonly']) ? 'readonly' : '' }}
         {{ !empty($field['disabled']) ? 'disabled' : '' }}
         {{ !empty($field['required']) ? 'required' : '' }}
         >
         @foreach ($field['options'] as $option)
-            <option value="{{ $option['value'] }}" {{ $option['value'] == old($field['name'], isset($model) ? $model->{$field['name']} : request()->{$field['name']}) ? 'selected' : '' }}>{{ $option['text'] }}</option>
+            <option value="{{ $option['value'] }}" {{ $option['value'] == old($field['name'], isset($model) ? data_get($model, $field['name']) : data_get(request(), $field['name'])) ? 'selected' : '' }}>{{ $option['text'] }}</option>
         @endforeach
     </select>
     @if ($errors->has($field['name']))
