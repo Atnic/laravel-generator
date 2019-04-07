44edd0232253279727b0bb43b8077e278b40159e
Farid Inawan <frdteknikelektro@gmail.com>
2019-04-08 00:37:58 +0700

    Little optimizing in PolicyMakeCommand

--- app/Console/Commands/PolicyMakeCommand.php
+++ app/Console/Commands/PolicyMakeCommand.php
@@ -18,32 +18,20 @@ class PolicyMakeCommand extends Command
      */
     public function handle()
     {
-        $name = $this->qualifyClass($this->getNameInput());
-
-        $path = $this->getPath($name);
-
-        // First we will check to see if the class already exists. If it does, we don't want
-        // to create the class and overwrite the user's code. So, we will bail out so the
-        // code is untouched. Otherwise, we will continue generating this class' files.
-        if ((! $this->hasOption('force') ||
-                ! $this->option('force')) &&
-            $this->alreadyExists($this->getNameInput())) {
-            $this->error($this->type.' already exists!');
-
-            return false;
-        }
-
-        // Next, we will generate the path to the location where this class' file should get
-        // written. Then, we will build the class and make the proper replacements on the
-        // stub files so that it gets the correctly formatted namespace and class name.
-        $this->makeDirectory($path);
-
         if (!$this->alreadyExists('ModelPolicy') && $this->hasOption('model')) {
-            $this->files->put($this->getPath($this->qualifyClass('ModelPolicy')), $this->buildClass($this->qualifyClass('ModelPolicy')));
-        }
-        $this->files->put($path, $this->buildClass($name));
+            $name = $this->qualifyClass('ModelPolicy');
 
-        $this->info($this->type.' created successfully.');
+            $path = $this->getPath($name);
+
+            // Next, we will generate the path to the location where this class' file should get
+            // written. Then, we will build the class and make the proper replacements on the
+            // stub files so that it gets the correctly formatted namespace and class name.
+            $this->makeDirectory($path);
+
+            $this->files->put($path, $this->buildClass($name));
+        }
+
+        return parent::handle();
     }
 
     /**
@@ -55,17 +43,19 @@ class PolicyMakeCommand extends Command
      */
     protected function buildClass($name)
     {
-        $stub = $this->files->get($this->getStub($name));
+        if ($name == $this->qualifyClass('ModelPolicy')) {
+            $stub = $this->files->get($this->getStub($name));
 
-        $parent = $this->replaceNamespace($stub, $name)->replaceClass($stub, $name);
+            $parent = $this->replaceNamespace($stub, $name)->replaceClass($stub, $name);
 
-        $stub = $this->replaceUserNamespace(
-            $parent //parent::buildClass($name)
-        );
+            $stub = $this->replaceUserNamespace($parent);
 
-        $model = $this->option('model');
+            $model = $this->option('model');
 
-        return $model ? $this->replaceModel($stub, $model) : $stub;
+            return $model ? $this->replaceModel($stub, $model) : $stub;
+        }
+
+        return parent::buildClass($name);
     }
 
     /**
