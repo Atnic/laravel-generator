0d93acaa53039079f2ae4c1c6a6dcc93c89ef195
Farid Inawan <frdteknikelektro@gmail.com>
2019-04-08 09:18:10 +0700

    Optimize Policy Auto Discovery

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -39,12 +39,14 @@ class AppServiceProvider extends ServiceProvider
         );
 
         Gate::before(function ($user, $ability, $arguments) {
-            $policy = Gate::getPolicyFor($arguments[0]);
-            if (is_null($policy) && is_string($class = (is_object($arguments[0]) ? get_class($arguments[0]) : $arguments[0]))) {
-                $classDirname = str_replace('/', '\\', dirname(str_replace('\\', '/', $class)));
-                $guessedPolicy = $classDirname.'\\Policies\\'.class_basename($class).'Policy';
-                if (class_exists($guessedPolicy))
-                    Gate::policy($class, $guessedPolicy);
+            foreach ($arguments as $argument) {
+                $policy = Gate::getPolicyFor($argument);
+                if (is_null($policy) && is_string($class = (is_object($argument) ? get_class($argument) : $argument))) {
+                    $classDirname = str_replace('/', '\\', dirname(str_replace('\\', '/', $class)));
+                    $guessedPolicy = $classDirname.'\\Policies\\'.class_basename($class).'Policy';
+                    if (class_exists($guessedPolicy))
+                        Gate::policy($class, $guessedPolicy);
+                }
             }
         });
     }
